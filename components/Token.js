8a62037ed994f12f538a6100e65a02a463963605
Will Spencer <willspencer805@gmail.com>
2022-12-08 17:40:22 -0500

    fix: showing the right image for the hackathon token

--- components/Token.js
+++ components/Token.js
@@ -5,6 +5,7 @@ import { useAccount } from "wagmi"
 import badge from "../public/badge.png"
 import styles from "../styles/Profile.module.css"
 import placeholder from "../public/dashed-box.png"
+import hack from "../public/hackathon.png"
 function Token({ tokenId, contractAddress, unique, abi, type }) {
   const [url, setUrl] = useState(null)
   const [name, setName] = useState(null)
@@ -14,11 +15,20 @@ function Token({ tokenId, contractAddress, unique, abi, type }) {
     "https://api.opensea.io/api/v2/metadata/matic/0x2953399124F0cBB46d2CbACD8A89cF0599974963/" +
     tokenId
 
+  let chainId
+  if (type == "hack") {
+    chainId = 80001
+  } else {
+    chainId = 137
+  }
+
   const contractRead = useContractRead({
     address: contractAddress,
     abi: abi,
     functionName: "balanceOf",
-    chainId: 137,
+    // chainId: type == "hack" ? 80001 : 137,
+    // chainId: 137,
+    chainId: chainId,
     args: unique ? [address] : [address, tokenId],
     onSuccess(data) {
       if (data > 0) {
@@ -48,13 +58,29 @@ function Token({ tokenId, contractAddress, unique, abi, type }) {
     if (type == "guild") {
       getData()
     } else if (type == "hack") {
-      console.log("TODO")
+      setUrl(
+        "https://sgf.infura-ipfs.io/ipfs/Qmecj5LzRMTtGksLyVrA4fagTJHknmeoxb7dn4JMJ2vyHP"
+      )
       setName("EY Blockchain Hackathon")
     } else {
-      setUrl("https://badge.eybc.xyz/img/placeholder.png")
       setName("EY Blockchain Badge")
     }
   }, [url])
+
+  if (type == "hack") {
+    return (
+      <div>
+        <Image
+          src={hack}
+          width={300}
+          height={300}
+          alt="nft"
+          className={styles.gridItem}
+        ></Image>
+        {name}
+      </div>
+    )
+  }
 
   if (unique && owned) {
     return (
