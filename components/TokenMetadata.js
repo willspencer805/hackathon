b779a23d664c4b715144a3ba4b4de8213c0b2c7b
Will Spencer <willspencer805@gmail.com>
2022-12-08 14:46:10 -0500

    fixed my messed up repo

--- components/TokenMetadata.js
+++ components/TokenMetadata.js
@@ -23,32 +23,29 @@ const TokenMetadata = () => {
   const handleSubmit = async (event) => {
     event.preventDefault()
 
-    // do something with the form data here
-    console.log(name, description, file)
-
-    const projectId = "2FdLREdMWVTzCZXvetszPIZLoJp"
-    const projectSecret = "ecd29287d3ee4d821db0347005875fc6"
+    const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+    const projectSecret = process.env.NEXT_PUBLIC_PROJECT_SECRET
 
     // First, add and pin image to IPFS
-    // const formData = new FormData()
-    // formData.append("file", file, "nft-image")
+    const formData = new FormData()
+    formData.append("file", file, "nft-image")
 
-    // try {
-    //   const res = await axios.post(
-    //     "https://ipfs.infura.io:5001/api/v0/add",
-    //     formData,
-    //     {
-    //       headers: {
-    //         "Content-Type": "multipart/form-data",
-    //         Authorization: "Basic " + btoa(`${projectId}:${projectSecret}`),
-    //       },
-    //     }
-    //   )
-
-    //   setIpfsHash(res.data.Hash)
-    // } catch (error) {
-    //   console.error(error)
-    // }
+    try {
+      const res = await axios.post(
+        "https://ipfs.infura.io:5001/api/v0/add",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: "Basic " + btoa(`${projectId}:${projectSecret}`),
+          },
+        }
+      )
+      console.log("IMAGE: ", res.data.Hash)
+      setIpfsHash(res.data.Hash)
+    } catch (error) {
+      console.error(error)
+    }
 
     // create json including name, description, and pinned QID
     const metadata = {
@@ -57,29 +54,30 @@ const TokenMetadata = () => {
       image: `sgf.infura-ipfs.io/ipfs/${ipfsHash}`,
     }
 
-    // const jsonForm = new FormData()
-    // const jsonBlob = new Blob([JSON.stringify(metadata)], {
-    //   type: "application/json",
-    // })
-    // jsonForm.append("json", jsonBlob, "token-metadata.json")
+    const jsonForm = new FormData()
+    const jsonBlob = new Blob([JSON.stringify(metadata)], {
+      type: "application/json",
+    })
+    jsonForm.append("json", jsonBlob, "token-metadata.json")
 
     // pin the json object to ipfs
-    // try {
-    //   const res = await axios.post(
-    //     "https://ipfs.infura.io:5001/api/v0/add",
-    //     jsonForm,
-    //     {
-    //       headers: {
-    //         "Content-Type": "multipart/form-data",
-    //         Authorization: "Basic " + btoa(`${projectId}:${projectSecret}`),
-    //       },
-    //     }
-    //   )
-    //   console.log(res.data.Hash)
-    // } catch (error) {
-    //   console.error(error)
-    // }
+    try {
+      const res = await axios.post(
+        "https://ipfs.infura.io:5001/api/v0/add",
+        jsonForm,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: "Basic " + btoa(`${projectId}:${projectSecret}`),
+          },
+        }
+      )
+      console.log(res.data.Hash)
+    } catch (error) {
+      console.error(error)
+    }
 
+    // TODO!!!!!!
     const addresses = [
       "0xA42Ca9055c8875972aDC1c7AB9F84783594f2e8B",
       "0xCAEe1AC3c58FFC6dB49ac963f73bbb09AF623bE7",
