8fad720a3e2a5e670af95748068d99577c6b8a8c
Will Spencer <willspencer805@gmail.com>
2022-12-15 16:02:37 -0500

    more prep

--- pages/api/mintToken.js
+++ pages/api/mintToken.js
@@ -38,8 +38,9 @@ const mintToken = nc({
 
   try {
     console.log("Minting token...")
-    const tx = contract.mintBatch(addresses, tokenId, amount)
-    // console.log(`https://mumbai.polygonscan.com/tx/${tx.hash}`)
+    res.status(200).json({ Response: { message: "Transaction completed!!" } })
+    const tx = await contract.mintBatch(addresses, tokenId, amount)
+    console.log(`https://mumbai.polygonscan.com/tx/${tx.hash}`)
     // const receipt = await tx.wait()
   } catch (error) {
     throw error
